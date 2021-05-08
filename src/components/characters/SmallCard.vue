319504fd37753b5ddb11919b2b6fccc8d6402dcb
GuruSam <artem7127.com@inbox.ru>
2021-05-08 15:50:33 +0500

    Copy password

--- src/components/characters/SmallCard.vue
+++ src/components/characters/SmallCard.vue
@@ -16,18 +16,25 @@
         </div>
       </div>
       <div class="card-footer py-3">
-        <form method="post" action="https://playlabirint.ru/game/login/login">
+        <form ref="authForm" method="post" action="https://playlabirint.ru/game/login/login">
           <input type="hidden" name="login" :value="character.name" />
           <input type="hidden" name="password" :value="character.password" />
           <input type="hidden" name="remember" value="1" />
           <input type="hidden" name="logout" value="1" />
           <input type="hidden" name="_xfRedirect" value="https://playlabirint.ru/game" />
           <input type="hidden" name="_xfToken" value="1620461050,09b979c1da62bb10b98dbe1a9d6f9d1e" />
-          <input type="submit" class="btn btn-sm btn-primary rounded-pill" value="Авторизоваться" />
         </form>
-        <!-- <a href="#" class="btn btn-sm btn-primary rounded-pill"><i class="ion ion-md-key"></i>&nbsp; Авторизоваться</a> -->
+        <a href="#" class="btn btn-sm btn-primary rounded-pill" @click="loginCharacter"><i class="ion ion-md-key"></i>&nbsp; Авторизоваться</a>
         &nbsp;
-        <a href="#" class="btn btn-sm rounded-pill"><i class="ion ion-md-copy"></i>&nbsp; Скопировать пароль</a>
+        <a 
+          href="#" 
+          class="btn btn-sm rounded-pill" 
+          :class="{ 'btn-success' : pwdCopied }"
+          @click="copyPassword"
+        >
+          <i class="ion" :class="pwdCopied ? 'ion-md-checkmark' : 'ion-md-copy'"></i>
+          &nbsp; Скопировать пароль
+        </a>
       </div>
     </div>
   </b-col>
@@ -47,7 +54,8 @@ export default {
 
     return {
       xlBreakpoint,
-      isXlEntered: xlBreakpoint.matches
+      isXlEntered: xlBreakpoint.matches,
+      pwdCopied: false
     }
   },
   mounted () {
@@ -67,6 +75,21 @@ export default {
         default:
           return 'Неизвестно'
       }
+    }
+  },
+  methods: {
+    loginCharacter () {
+      this.$refs.authForm.submit()
+    },
+    copyPassword () {
+      navigator.clipboard.writeText(this.character.password)
+        .then(() => {
+          this.pwdCopied = true
+
+          setTimeout(() => {
+            this.pwdCopied = false
+          }, 10000);
+        })
     }
   }
 }
