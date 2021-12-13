a3a2b16e4be515ad02879c72d284c5b290ede3fb
GuruSam <artem7127.com@inbox.ru>
2021-12-13 14:34:39 +0500

    Notification close icon and adaptive width

--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -1,21 +1,23 @@
 <template>
-  <transition name="fade-left">
-    <notifications group="notifications" width="350px" :duration="-1">
-      <template slot="body" slot-scope="props">
-        <div class="media bg-dark text-white px-3 py-4 mb-2" :class="getBorderClass(props.item.type)" @click="props.close">
-          <div class="media align-items-center w-100">
-            <div class="mr-3">
-              <span class="display-4 ion" :class="getIcon(props.item.type)"></span>
-            </div>
-            <div class="media-body">
-              <div class="notification-title" v-if="props.item.title">{{ props.item.title }}</div>
-              {{ props.item.text }}
-            </div>
+  <notifications group="notifications" :duration="5000" animation-name="v-fade-left" :closeOnClick="false">
+    <template slot="body" slot-scope="props">
+      <div class="media bg-dark text-white pl-3 pr-4 py-4 mb-2" :class="getBorderClass(props.item.type)" @click="props.close">
+        <div class="media align-items-center w-100">
+          <div class="mr-3">
+            <span class="display-4 ion" :class="getIcon(props.item.type)"></span>
+          </div>
+          <div class="media-body">
+            <div class="notification-title" v-if="props.item.title">{{ props.item.title }}</div>
+            {{ props.item.text }}
           </div>
         </div>
-      </template>
-    </notifications>
-  </transition>
+        <span 
+          class="h3 ion ion-ios-close close-icon" 
+          @click="props.close"
+        ></span>
+      </div>
+    </template>
+  </notifications>
 </template>
 
 <style src="@/vendor/libs/vue-notification/vue-notification.scss" lang="scss"></style>
@@ -50,6 +52,19 @@ export default {
 </script>
 
 <style>
+@media (min-width: 360px) {
+  .vue-notification-group {
+    width: 350px !important;
+  }
+}
+
+@media (max-width: 359px) {
+  .vue-notification-group {
+    width: 100% !important;
+    margin: 0.375rem 0 !important;
+  }
+}
+
 .border-danger {
   border-left: 3px solid rgb(105, 4, 4);
 }
@@ -68,5 +83,13 @@ export default {
 .v-fade-left-leave-to {
   opacity: 0;
   transform: translateX(500px) scale(0.2);
+}
+
+.close-icon {
+  position: absolute;
+  top: 0;
+  right: 5px;
+  padding: 5px;
+  cursor: pointer;
 }
 </style>
