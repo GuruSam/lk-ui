b33be0e6434946d70ee496ec67161e65d7bbabc9
GuruSam <artem7127.com@inbox.ru>
2021-12-13 14:49:33 +0500

    Login as character in new tab

--- src/components/characters/AuthButtons.vue
+++ src/components/characters/AuthButtons.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <form ref="authForm" method="post" action="https://playlabirint.ru/game/login/login">
+    <form ref="authForm" method="post" action="https://playlabirint.ru/game/login/login" target="_blank">
       <input type="hidden" name="login" :value="character.name" />
       <input type="hidden" name="password" :value="character.password" />
       <input type="hidden" name="remember" value="1" />
