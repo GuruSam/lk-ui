8ee5d2a657a7d34aa92e2196da00679879e8a7e0
GuruSam <artem7127.com@inbox.ru>
2022-03-23 09:57:36 +0500

    Sidenav swipe logic improved

--- src/components/layout/Sidenav.vue
+++ src/components/layout/Sidenav.vue
@@ -82,17 +82,12 @@ export default {
 
   mounted () {
     if (this.layoutHelpers.isSmallScreen()) {
-      // const sideNav = this.layoutHelpers.getSidenav()
-      // const width = sideNav.getBoundingClientRect().width
-
       let clientX = null
       let clientY = null
       let xDiff = null
       let yDiff = null
-      // let transformed = false
       let isCollapsed = this.layoutHelpers.isCollapsed()
       let moves = 0
-
       let timeout = null
 
       document.addEventListener('touchstart', evt => {
@@ -108,10 +103,9 @@ export default {
 
           if (Math.abs(xDiff) > Math.abs(yDiff) && xDiff !== 0) {
             timeout = setTimeout(() => {
-              console.log(xDiff)
               if (xDiff >= 200 && isCollapsed) {
                 this.layoutHelpers.setCollapsed(false)
-              } else if (xDiff <= -200 && !isCollapsed) {
+              } else if (xDiff <= -150 && !isCollapsed) {
                 this.layoutHelpers.setCollapsed(true)
               }
 
@@ -130,44 +124,6 @@ export default {
         clientY = 0
         moves = 0
       })
-
-      // document.addEventListener('touchmove', evt => {
-      //   xDiff = evt.touches[0].clientX - clientX
-
-      //   if (moves === 0 && isCollapsed) {
-      //     yDiff = evt.touches[0].clientY - clientY
-
-      //     if (Math.abs(xDiff) > Math.abs(yDiff) && xDiff > 0) {
-      //       transformed = true
-      //     }
-
-      //     moves++
-      //   }
-
-      //   if (transformed) {
-      //     sideNav.style.transform = `translate3d(-${width - xDiff}px, 0, 0)`
-      //   }
-      // })
-
-      // document.addEventListener('touchend', () => {
-      //   if (transformed) {
-      //     sideNav.style.transform = `translate3d(-${width}px, 0, 0)`
-
-      //     if (xDiff >= 200) {
-      //       this.layoutHelpers.setCollapsed(false)
-      //     } else {
-      //       this.layoutHelpers.setCollapsed(true)
-      //     }
-
-      //     clientX = 0
-      //     clientY = 0
-      //     xDiff = 0
-      //     yDiff = 0
-      //     transformed = false
-      //   }
-
-      //   moves = 0
-      // })
 
       this.layoutHelpers.on('toggle', () => 
         isCollapsed = this.layoutHelpers.isCollapsed()
