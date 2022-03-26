9ed277b9c290ee1efb6abc509eb62b6b7057907b
GuruSam <artem7127.com@inbox.ru>
2022-03-26 10:59:46 +0500

    App footer

---
+++ src/components/layout/AppFooter.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer class="layout-footer footer">
+    <div class="container-fluid d-flex flex-wrap justify-content-between text-center container-p-x pb-3">
+      <div class="pt-3 text-primary">
+        2015-2022 © <a class="footer-text font-weight-bolder text-primary" href="https://thesims.cc" target="_blank">The Sims Creative Club</a>
+      </div>
+
+      <div>
+        <a href="https://playlabirint.ru/rules" class="footer-link pt-3">Правила игры</a>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'AppFooter'
+}
+</script>
+
+<style>
+.layout-footer {
+  background-color: #131315;
+}
+</style>
