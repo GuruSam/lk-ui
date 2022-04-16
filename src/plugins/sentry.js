81b3d5db6a59099c5ac9e6b87d529ea5024e1a44
GuruSam <artem7127.com@inbox.ru>
2022-04-16 19:30:09 +0500

    Sentry setup

---
+++ src/plugins/sentry.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import * as Sentry from '@sentry/vue'
+import { BrowserTracing } from '@sentry/tracing'
+import router from '@/router'
+
+Sentry.init({
+  Vue,
+  dsn: 'https://6eb911f211ab431fb9e128953d0c7654@o1206780.ingest.sentry.io/6340307',
+  integrations: [
+    new BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracingOrigins: ['localhost', 'lk.playlabirint.ru', /^\//],
+    }),
+  ],
+  tracesSampleRate: 1.0,
+})
