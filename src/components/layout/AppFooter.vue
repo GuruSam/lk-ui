3f1b7637f7087a4312e6622260440a5929e815de
GuruSam <artem7127.com@inbox.ru>
2022-04-22 20:00:35 +0500

    Telegram link in footer

--- src/components/layout/AppFooter.vue
+++ src/components/layout/AppFooter.vue
@@ -1,12 +1,15 @@
 <template>
   <footer class="layout-footer footer">
-    <div class="container-fluid d-flex flex-wrap justify-content-between text-center container-p-x pb-3">
-      <div class="pt-3 text-primary">
+    <div class="container-fluid d-flex flex-wrap justify-content-between text-center container-p-x pb-3 pt-3">
+      <div class="text-primary">
         2015-2022 © <a class="footer-text font-weight-bolder text-primary" href="https://thesims.cc" target="_blank">The Sims Creative Club</a>
       </div>
 
-      <div>
-        <a href="https://playlabirint.ru/rules" class="footer-link pt-3">Правила игры</a>
+      <div class="links d-flex align-items-center">
+        <a class="tg-link mr-3" href="https://t.me/labirint_news" target="_blank" title="Новости Лабиринта">
+          <span class="sr-only">Новости Лабиринта</span>
+        </a>
+        <a href="https://playlabirint.ru/rules" class="footer-link">Правила игры</a>
       </div>
     </div>
   </footer>
@@ -21,5 +24,13 @@ export default {
 <style>
 .layout-footer {
   background-color: #131315;
+}
+
+.tg-link {
+  display: inline-block;
+  width: 21px;
+  height: 21px;
+  background-image: url(/telegram-plane.png);
+  background-size: cover;
 }
 </style>
