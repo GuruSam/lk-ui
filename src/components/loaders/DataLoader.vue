1f8c6f474c139baf87d4a1c1b87a11cfe16b3106
GuruSam <artem7127.com@inbox.ru>
2022-11-09 21:38:16 +0500

    Spinner size variant with props

--- src/components/loaders/DataLoader.vue
+++ src/components/loaders/DataLoader.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="d-flex justify-content-center mb-4 mt-4">
-    <div class="spinner-border text-primary">
+  <div class="d-flex justify-content-center align-items-center mb-4 mt-4">
+    <div class="spinner-border text-primary" :class="{'spinner-border-sm' : small}">
       <span class="sr-only">Загружаю...</span>
     </div>
   </div>
@@ -8,6 +8,13 @@
 
 <script>
 export default {
-  name: 'DataLoader'
+  name: 'DataLoader',
+  
+  props: {
+    small: {
+      type: Boolean,
+      default: false
+    }
+  }
 }
 </script>
